39866f8d39c4b39784250007298045378b148942
FerghusSmith <24311fs@hvhs.school.nz>
2026-06-12 02:55:39 +0000

    Jun 12, 2026, 2:55 AM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello world!")
+
+let ArraysArray = ["GyrosArray","SouvlakiArray"]
+let GyrosArray = ["chicken gyros","beef gyros","pork gyros","mixed gyros"];
+let SouvlakiArray = ["chicken souvlaki","beef souvlaki","pork souvlaki","mixed souvlaki"];
+
+function addToCart(_itemGroup, _item, _price){
+    
+}
