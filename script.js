3e9ad6280d135922fad91c75d1622aaaa79ca32c
FerghusSmith <24311fs@hvhs.school.nz>
2026-06-12 03:09:05 +0000

    Jun 12, 2026, 3:09 AM

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 console.log("Hello world!")
 
-let ArraysArray = ["GyrosArray","SouvlakiArray"]
+let GroupArray = ["GyrosArray","SouvlakiArray"]
 let GyrosArray = ["chicken gyros","beef gyros","pork gyros","mixed gyros"];
 let SouvlakiArray = ["chicken souvlaki","beef souvlaki","pork souvlaki","mixed souvlaki"];
 
 function addToCart(_itemGroup, _item, _price){
     console.log("running addToCart")
-    console.log(ArraysArray[0][0])
+    let itemGroup = GroupArray[0];
+    console.log(itemgroup[0])
 }
