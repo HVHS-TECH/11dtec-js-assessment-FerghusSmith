13dcc9b73bbf76d7d96311d0cf58a9c983273966
FerghusSmith <24311fs@hvhs.school.nz>
2026-06-12 03:11:19 +0000

    Jun 12, 2026, 3:11 AM

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log("Hello world!")
 
-let GroupArray = ["GyrosArray","SouvlakiArray"]
+let GroupArray = ["GyrosArray","SouvlakiArray"];
 let GyrosArray = ["chicken gyros","beef gyros","pork gyros","mixed gyros"];
 let SouvlakiArray = ["chicken souvlaki","beef souvlaki","pork souvlaki","mixed souvlaki"];
 
 function addToCart(_itemGroup, _item, _price){
-    console.log("running addToCart")
-    let itemGroup = GroupArray[0];
-    console.log(itemgroup[0])
+    console.log("running addToCart");
+    let itemGroup = GroupArray[1];
+    console.log(itemGroup[3]);
 }
