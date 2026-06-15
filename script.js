4950e62e2db043c4f6a4f0fc6a7e5e6707f706b8
FerghusSmith <24311fs@hvhs.school.nz>
2026-06-15 21:28:23 +0000

    Jun 15, 2026, 9:28 PM

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 console.log("Hello world!")
 
-let GroupArray = ["GyrosArray","SouvlakiArray"];
-let GyrosArray = ["chicken gyros","beef gyros","pork gyros","mixed gyros"];
-let SouvlakiArray = ["chicken souvlaki","beef souvlaki","pork souvlaki","mixed souvlaki"];
+let GroupArray = [      /* layout ["food name", food cost] */
+/* Gyros group */       [["Chicken Gyros", 20], ["Beef Gyros", 20], ["Pork Gyros", 20], ["Mixed Gyros", 20]], 
+/* Souvlaki group */    [["Chicken Souvlaki", 20], ["Beef Souvlaki", 20], ["Pork Souvlaki", 20], ["Mixed Souvlaki", 20]],
+/* Drinks group */      [["Coca-cola", 3], ["Sprite", 3], ["L&P", 3], ["Diet Coke", 2]],
+/* Sides group */       [["Hot Chips w Tzatziki", 6], ["Calamari", 8]],
+/* Vegetables group */  [["Greek salad", 15], ["Mousaka", 15], ["Yemista", 10]],
+/* Dessert group */     [["Baklava", 5], ["Galaktoboureko", 5], ["Revani", 5], ["Ice cream", 5]]
+];
 
 function addToCart(_itemGroup, _item, _price){
     console.log("running addToCart");
