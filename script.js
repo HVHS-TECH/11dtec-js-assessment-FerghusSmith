277c0f7bab816a75638c24db73d2734976b13dbb
FerghusSmith <24311fs@hvhs.school.nz>
2026-06-16 23:50:18 +0000

    Jun 16, 2026, 11:50 PM

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let GroupArray = [
         {item: "Hot Chips w Tzatziki", cost: 6, order: 0},
         {item: "Calamari", cost: 8, order: 0}
     ],
-    /*vegetables*/[
+    /*vege*/[
         {item: "Greek salad", cost: 15, order: 0},
         {item: "Mousaka", cost: 15, order: 0},
         {item: "Yemista", cost: 10, order: 0}
@@ -44,4 +44,15 @@ function addToCart(_itemGroup, _itemInfo){
     totalCost = totalCost + thisItem.cost;
     console.log("you have ordered "+thisItem.order+" "+thisItem.item+" for $"+(thisItem.cost*thisItem.order));
     console.log("the total cost of your cart is $"+totalCost);
+}
+function dropDown(_menu){
+    console.log("running function dropDown");
+    _menu.innerHTML = "<p>Baklava</p>
+        <button onsubmit='return false;' onclick=addToCart(5,0)>add to cart</button>
+        <p>Galaktoboureko</p>
+        <button onsubmit='return false;' onclick=addToCart(5,1)>add to cart</button>
+        <p>Revani</p>
+        <button onsubmit='return false;' onclick=addToCart(5,2)>add to cart</button>
+        <p>Ice cream</p>
+        <button onsubmit='return false;' onclick=addToCart(5,3)>add to cart</button>";
 }
